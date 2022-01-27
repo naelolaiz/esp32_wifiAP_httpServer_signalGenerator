2a257ec80b6b49217a511c73e2fdff3dced2999e
nael <naelblog@gmail.com>
2022-01-27 19:05:27 +0100

    more

--- src/NaelServer.h
+++ src/NaelServer.h
@@ -2,69 +2,51 @@
 #define __NAEL_SERVER_H__
 
 // http
-extern "C" 
-{
-    #include <esp_http_server.h>
+extern "C" {
+#include <esp_http_server.h>
 }
-// based on https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/protocols/esp_http_server.html
+// based on
+// https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/protocols/esp_http_server.html
 #include "NaelCppLibrary.h"
-#include <driver/gpio.h> // TODO: remove
 #include <array>
+#include <driver/gpio.h> // TODO: remove
+class Server {
+private:
+  static FormForLed mFormForLed;
+  httpd_config_t mConfig = HTTPD_DEFAULT_CONFIG();
+  /* Empty handle to esp_http_server */
+  httpd_handle_t mServer{nullptr};
 
+  /* URI handler structure for GET /uri */
+  httpd_uri_t uri_get = {.uri = "/uri",
+                         .method = HTTP_GET,
+                         .handler = get_handler,
+                         .user_ctx = this};
 
-namespace Server
-{
-   class Server
-   {
-   private:
-   static FormForLed mFormForLed;
-    httpd_config_t mConfig = HTTPD_DEFAULT_CONFIG();
-    /* Empty handle to esp_http_server */
-    httpd_handle_t mServer{nullptr};
+  /* URI handler structure for POST /uri */
+  httpd_uri_t uri_post = {.uri = "/uri",
+                          .method = HTTP_POST,
+                          .handler = post_handler,
+                          .user_ctx = this};
 
-   /* URI handler structure for GET /uri */
-    httpd_uri_t uri_get = {
-       .uri      = "/uri",
-       .method   = HTTP_GET,
-       .handler  = get_handler,
-       .user_ctx = this
-   };
-   
-   /* URI handler structure for POST /uri */
-   httpd_uri_t uri_post = {
-       .uri      = "/uri",
-       .method   = HTTP_POST,
-       .handler  = post_handler,
-       .user_ctx = this
-   };
-   
-   /* URI handler structure for GET /form */
-   httpd_uri_t form_get = {
-       .uri      = "/form",
-       .method   = HTTP_GET,
-       .handler  = get_handler,
-       .user_ctx = this
-   };
-   
-   /* URI handler structure for POST /form */
-   httpd_uri_t form_post = {
-       .uri      = "/form",
-       .method   = HTTP_POST,
-       .handler  = post_handler,
-       .user_ctx = this
-   };
+  /* URI handler structure for GET /form */
+  httpd_uri_t form_get = {.uri = "/form",
+                          .method = HTTP_GET,
+                          .handler = get_handler,
+                          .user_ctx = this};
 
-   
-       /* data */
-   public:
-       Server(/* args */) = default;
-       static esp_err_t get_handler(httpd_req_t *req);
-       static esp_err_t post_handler(httpd_req_t *req);
+  /* URI handler structure for POST /form */
+  httpd_uri_t form_post = {.uri = "/form",
+                           .method = HTTP_POST,
+                           .handler = post_handler,
+                           .user_ctx = this};
 
-   httpd_handle_t start_webserver(void);
-   
-   void stop_webserver(httpd_handle_t server);
-   };
-}
+public:
+  Server(/* args */) = default;
+  static esp_err_t get_handler(httpd_req_t *req);
+  static esp_err_t post_handler(httpd_req_t *req);
+  httpd_handle_t start_webserver(void);
+  void stop_webserver(httpd_handle_t server);
+};
 
 #endif // __NAEL_SERVER_H__
